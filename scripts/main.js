b35d0e024b806808ca511e1b173bae62e9f9557b
Andrew Smith <meuzishun@gmail.com>
2025-05-19 18:25:29 -0400

    Remove comments from main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 import { Piano } from './modules/pianoClass.js';
-import { Melody } from './modules/melodyClass.js'; // The melody takes the score, the composer's chosen gestures, and the 'Glued' setting and creates a sequence of Notes with absolute MIDI pitch numbers and durations in beats
-import { player } from './modules/player.js'; // The player can of course play notes, melodies, chords, progressions, etc... but also schedules a melody (sequence of notes with MIDI pitch numbers and beat durations) by giving each note a start and end time relative to a GO point
+import { Melody } from './modules/melodyClass.js';
+import { player } from './modules/player.js';
 import { computerKeyboardInput } from './modules/computerKeyboardInput.js';
 
 const newGestureButton = document.querySelector('.newGesture-btn');
