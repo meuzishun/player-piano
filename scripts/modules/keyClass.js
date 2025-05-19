f7b85bc9be901de6da89537e728078bb84a39d41
Andrew Smith <meuzishun@gmail.com>
2025-05-19 18:06:23 -0400

    Format keyClass.js

--- scripts/modules/keyClass.js
+++ scripts/modules/keyClass.js
@@ -1,79 +1,80 @@
 import { Tone } from '../modules/toneClass.js';
 
 class Key {
-    constructor(midi, piano) {
-        this.midi = midi;
-        this.piano = piano;
-        this.filePath = `../audio/${this.midi}.mp3`;
-        this.setPitchClass();
-        this.setKeyColor();
-        this.loadSample();
-        this.buildElement();
-    }
+  constructor(midi, piano) {
+    this.midi = midi;
+    this.piano = piano;
+    this.filePath = `../audio/${this.midi}.mp3`;
+    this.setPitchClass();
+    this.setKeyColor();
+    this.loadSample();
+    this.buildElement();
+  }
 
-    setPitchClass() {
-        this.pitchClass = this.midi % 12;
-    }
+  setPitchClass() {
+    this.pitchClass = this.midi % 12;
+  }
 
-    setKeyColor() {
-        const WhPCs = [0, 2, 4, 5, 7, 9, 11];
-        this.color = WhPCs.includes(this.pitchClass) ? 'white' : 'black';
-    }
+  setKeyColor() {
+    const WhPCs = [0, 2, 4, 5, 7, 9, 11];
+    this.color = WhPCs.includes(this.pitchClass) ? 'white' : 'black';
+  }
 
-    async loadSample() {
-        this.buffer = await this.getFile(this.piano.audioCtx, this.filePath);
-    }
+  async loadSample() {
+    this.buffer = await this.getFile(this.piano.audioCtx, this.filePath);
+  }
 
-    async getFile(audioContext, filepath) {
-        const response = await fetch(filepath);
-        const arrayBuffer = await response.arrayBuffer();
-        const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
-        audioBuffer.pitch = this.midi;
-        this.piano.numberOfAudioFiles++;
-        const loadingBar = document.querySelector('.fill');
-        loadingBar.style.width = (this.piano.numberOfAudioFiles / this.piano.numOfKeys) * 100 + '%';
-        if (this.piano.numberOfAudioFiles === this.piano.numOfKeys) {
-            this.piano.entireContainer.classList.remove('hidden');
-        }
-        return audioBuffer;
+  async getFile(audioContext, filepath) {
+    const response = await fetch(filepath);
+    const arrayBuffer = await response.arrayBuffer();
+    const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+    audioBuffer.pitch = this.midi;
+    this.piano.numberOfAudioFiles++;
+    const loadingBar = document.querySelector('.fill');
+    loadingBar.style.width =
+      (this.piano.numberOfAudioFiles / this.piano.numOfKeys) * 100 + '%';
+    if (this.piano.numberOfAudioFiles === this.piano.numOfKeys) {
+      this.piano.entireContainer.classList.remove('hidden');
     }
+    return audioBuffer;
+  }
 
-    buildElement() {
-        const element = document.createElement('div');
-        element.classList.add('key', `${this.color}-key`);
-        element.dataset.midi = this.midi;
-        element.dataset.pitchClass = this.pitchClass;
-        element.creator = this; //? Creates a reference to the object that birthed the element... circular but makes events easier to manage.  Is this a problem?
-        this.element = element;
-    }
+  buildElement() {
+    const element = document.createElement('div');
+    element.classList.add('key', `${this.color}-key`);
+    element.dataset.midi = this.midi;
+    element.dataset.pitchClass = this.pitchClass;
+    element.creator = this; //? Creates a reference to the object that birthed the element... circular but makes events easier to manage.  Is this a problem?
+    this.element = element;
+  }
 
-    pressedKey(startTime) {
-        this.playPitch(startTime);
-        this.pressedColor();
-    }
-    
-    liftedKey(stopTime) {
-        this.silencePitch(stopTime);
-        this.liftedColor();
-    }
+  pressedKey(startTime) {
+    this.playPitch(startTime);
+    this.pressedColor();
+  }
 
-    pressedColor() {
-        this.element.classList.add(`${this.color}-key-pressed`);
-    }
+  liftedKey(stopTime) {
+    this.silencePitch(stopTime);
+    this.liftedColor();
+  }
 
-    liftedColor() {
-        this.element.classList.remove(`${this.color}-key-pressed`);
-    }
+  pressedColor() {
+    this.element.classList.add(`${this.color}-key-pressed`);
+  }
 
-    playPitch(startTime) {
-        this.tone = new Tone(this.buffer, this.piano);
-        this.tone.play(startTime || this.piano.audioCtx.currentTime);
-    }
+  liftedColor() {
+    this.element.classList.remove(`${this.color}-key-pressed`);
+  }
 
-    silencePitch(stopTime) {
-        this.tone.stop(stopTime || this.piano.audioCtx.currentTime);
-        this.tone = null;
-    }
+  playPitch(startTime) {
+    this.tone = new Tone(this.buffer, this.piano);
+    this.tone.play(startTime || this.piano.audioCtx.currentTime);
+  }
+
+  silencePitch(stopTime) {
+    this.tone.stop(stopTime || this.piano.audioCtx.currentTime);
+    this.tone = null;
+  }
 }
 
-export { Key }
+export { Key };
