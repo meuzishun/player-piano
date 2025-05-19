319f6ab2f2606a4fb39a7f132c6d9789457b2d0c
Andrew Smith <meuzishun@gmail.com>
2025-05-19 18:06:46 -0400

    Format inputData.js

--- scripts/modules/inputData.js
+++ scripts/modules/inputData.js
@@ -1,48 +1,50 @@
-import { pitchSequences } from '../modules/sequences.js'
+import { pitchSequences } from '../modules/sequences.js';
 
 class InputData {
-    constructor () {
-        this.collectTonic();
-        this.collectMeter();
-        this.collectTempo();
-        this.collectNumberOfGestures();
-        this.collectMatchedEnds();
-        this.collectGlued();
-        this.collectBoxes();
-    }
+  constructor() {
+    this.collectTonic();
+    this.collectMeter();
+    this.collectTempo();
+    this.collectNumberOfGestures();
+    this.collectMatchedEnds();
+    this.collectGlued();
+    this.collectBoxes();
+  }
 
-    collectTonic() {
-        let letterInput = +document.querySelector('#tonic-letter').value;
-        let octaveInput = +document.querySelector('#tonic-octave').value;
-        this.tonic = (12 * octaveInput) + letterInput + 12;
-    }
+  collectTonic() {
+    let letterInput = +document.querySelector('#tonic-letter').value;
+    let octaveInput = +document.querySelector('#tonic-octave').value;
+    this.tonic = 12 * octaveInput + letterInput + 12;
+  }
 
-    collectMeter() {
-        this.meter = document.querySelector('#meter').value;
-    }
+  collectMeter() {
+    this.meter = document.querySelector('#meter').value;
+  }
 
-    collectTempo() {
-        this.tempo = +document.querySelector('#tempo-field').value;
-    }
+  collectTempo() {
+    this.tempo = +document.querySelector('#tempo-field').value;
+  }
 
-    collectNumberOfGestures() {
-        this.numberOfGestures = +document.querySelector('#gesture-num').value;
-    }
+  collectNumberOfGestures() {
+    this.numberOfGestures = +document.querySelector('#gesture-num').value;
+  }
 
-    collectMatchedEnds() {
-        this.matchedEnds = document.querySelector('#matched-checkbox').checked;
-    }
+  collectMatchedEnds() {
+    this.matchedEnds = document.querySelector('#matched-checkbox').checked;
+  }
 
-    collectGlued() {
-        this.glued = document.querySelector('#glued-checkbox').checked;
-    }
+  collectGlued() {
+    this.glued = document.querySelector('#glued-checkbox').checked;
+  }
 
-    collectBoxes() {
-        let gestureBoxes = Array.from(document.querySelectorAll('.gesture-box'));
-        let checkedBoxes = gestureBoxes.filter(box => box.checked);
-        let arrayOfChoices = checkedBoxes.map(box => pitchSequences[box.dataset.mode][box.dataset.gesture]).flat();
-        this.checkedBoxes = arrayOfChoices;
-    }
+  collectBoxes() {
+    let gestureBoxes = Array.from(document.querySelectorAll('.gesture-box'));
+    let checkedBoxes = gestureBoxes.filter((box) => box.checked);
+    let arrayOfChoices = checkedBoxes
+      .map((box) => pitchSequences[box.dataset.mode][box.dataset.gesture])
+      .flat();
+    this.checkedBoxes = arrayOfChoices;
+  }
 }
 
-export { InputData }
+export { InputData };
