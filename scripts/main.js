5570072fdc12eb0d32c0a2da967c7263be2fc9cc
Andrew Smith <meuzishun@gmail.com>
2025-05-30 11:00:08 -0400

    Use teacher module in newGestureButton click handler

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ import { Piano } from './modules/pianoClass.js';
 import { Melody } from './modules/melodyClass.js';
 import { player } from './modules/player.js';
 import { computerKeyboardInput } from './modules/computerKeyboardInput.js';
+import teacher from './modules/teacher.js';
 
 const newGestureButton = document.querySelector('.newGesture-btn');
 const playButton = document.querySelector('.play-btn');
@@ -12,8 +13,21 @@ const tonicOctave = document.querySelector('#tonic-octave');
 player.piano = new Piano(73, 24);
 
 newGestureButton.addEventListener('click', () => {
-  player.melody = new Melody();
+  teacher.ignore();
+  teacher.clearQuestion();
+  teacher.clearFeedback();
+
+  const melody = new Melody();
+  teacher.setQuestion(melody);
+  player.melody = melody;
+
   player.playMelody();
+  const waitTime = Math.round(melody.notes.at(-2).endTime * 1000);
+
+  const waitForGesture = setTimeout(() => {
+    teacher.listen();
+    clearTimeout(waitForGesture);
+  }, waitTime);
 });
 
 playButton.addEventListener('click', () => {
